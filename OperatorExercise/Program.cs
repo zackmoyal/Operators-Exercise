c2fcb11f79e855caacc1fefba4b85e8f1c56d84d
Zack Moyal <zack.moyal@gmail.com>
2023-12-30 01:11:42 -0800

    Exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //---Exercise 1---
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}+{b} is {sum}.");
+            Console.WriteLine($"{a}-{b} is {difference}.");
+            Console.WriteLine($"{a}*{b} is {product}.");
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            Console.WriteLine($"{a}%{b} is {remainder}.");
         }
     }
 }
